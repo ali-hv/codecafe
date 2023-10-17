322fd0ad47d5f806b939b0502892a022361aa299
ali-hv <alihv5000@gmail.com>
2023-10-17 18:27:25 +0330

    load admin panel url from env variable

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+import os
+
+if 'ADMIN_PANEL_URL' in os.environ:
+    admin_panel_url = os.environ['ADMIN_PANEL_URL']
+else:
+    admin_panel_url = 'admin/'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_panel_url, admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
     path('courses/', include('courses.urls')),
