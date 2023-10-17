41ac1a95cfae7618207c0a146f65025f57c779a3
ali-hv <alihv5000@gmail.com>
2023-10-17 18:41:23 +0330

    remove an additional comment

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ urlpatterns = [
     path('user/', include('users.urls')),
     path('blog/', include('blog.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
