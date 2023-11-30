7a46d0096d8bd97bc812fb4d58f50a52d3978374
ali-hv <alihv5000@gmail.com>
2023-12-01 00:18:55 +0330

    register UserCart to admin panel

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from cart.models import UserCart
 
-# Register your models here.
+
+@admin.register(UserCart)
+class ReplyCommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "get_payable_amount", ]
