2251e0c6d27d74dda67088b0ffebf7f9a876e1f3
ali-hv <alihv5000@gmail.com>
2023-11-30 23:22:47 +0330

    create UserCart and CartCourse models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import CustomUser
+from courses.models import Course
 
-# Create your models here.
+
+class CartCourse(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class UserCart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    courses = models.ForeignKey(CartCourse, on_delete=models.CASCADE)
+
+    def get_payable_amount(self):
+        amount = 0
+        for cart_course in self.courses.all():
+            amount += cart_course.course.price
+
+        return amount
