99c39f7460f81db444920b08c89525155faf5076
ali-hv <alihv5000@gmail.com>
2023-11-30 23:41:37 +0330

    create add_to_cart url

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
+from .views import add_to_cart
 
 app_name = 'cart'
 
 urlpatterns = [
-
+    path("add-to-cart/<int:course_id>", add_to_cart, name="add_to_cart")
 ]
