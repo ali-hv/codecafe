2149a25d41266269c02ba10bfca1d13fb82e57ce
ali-hv <alihv5000@gmail.com>
2023-12-06 00:31:40 +0330

    create pay and delete_course_from cart views

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from cart.models import UserCart
 from courses.models import Course
+from payment.views import go_to_gateway_view
 
 
 @login_required
@@ -18,3 +20,22 @@ def add_to_cart(request, course_id):
 def cart(request):
     user_cart = get_object_or_404(UserCart, user=request.user)
     return render(request, 'cart/cart.html', {"cart": user_cart})
+
+
+@login_required
+def pay(request):
+    return go_to_gateway_view(request)
+
+
+@login_required
+def delete_course_from_cart(request):
+    if request.method == "POST":
+        course_id = request.POST.get('course_id')
+        user_cart = get_object_or_404(UserCart, user=request.user)
+        course = get_object_or_404(Course, id=course_id)
+        user_cart.courses.remove(course)
+        user_cart.save()
+
+        return JsonResponse({'message': 'Course removed successfully'})
+
+    return JsonResponse({'message': 'Invalid request method'}, status=400)
