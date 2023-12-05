c28b0a0983038581d33457f8a4c46265bb65414e
ali-hv <alihv5000@gmail.com>
2023-12-06 00:32:18 +0330

    add pay and delete-course-from-cart url paths

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
-from .views import add_to_cart, cart
+from .views import add_to_cart, cart, pay, delete_course_from_cart
 
 app_name = 'cart'
 
 urlpatterns = [
     path("", cart, name="cart"),
+    path("pay", pay, name="pay"),
     path("add-to-cart/<int:course_id>", add_to_cart, name="add_to_cart"),
+    path("delete-course-from-cart/", delete_course_from_cart, name="delete_course_from_cart"),
 ]
