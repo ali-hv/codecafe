9c246a74d6968d7dcab6010158c44ae2fa02689e
ali-hv <alihv5000@gmail.com>
2023-12-06 13:57:16 +0330

    change a view name

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, re_path
-from .views import courses_list, course_detail, register_course, search_courses
+from .views import courses_list, course_detail, register_free_course, search_courses
 
 app_name = 'courses'
 
 urlpatterns = [
     path('', courses_list, name="courses_list"),
-    path('register/<int:course_id>', register_course, name='register_course'),
+    path('register/<int:course_id>', register_free_course, name='register_free_course'),
     re_path(r'^search(?P<keyword>\w{0,50})/$', search_courses, name='search_courses'),
     re_path(r'(?P<course_slug>[-\w]+)/', course_detail, name="course_detail"),
 ]
