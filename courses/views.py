b4c65e59fa9b2e2133eaa402b580666cad47a2f3
ali-hv <alihv5000@gmail.com>
2023-12-06 13:55:22 +0330

    create register_items and delete_failed_temproray_order views

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from cart.models import UserCart
+from cart.models import UserCart, TemporaryOrder
 from .models import Course
 
 
@@ -23,7 +23,7 @@ def course_detail(request, course_slug):
     return render(request, 'courses/course.html', context=context)
 
 
-def register_course(request, course_id):
+def register_free_course(request, course_id):
     if request.user.is_authenticated:
         course_user_field = get_object_or_404(Course, id=course_id)
         course_user_field.users.add(request.user)
@@ -38,3 +38,27 @@ def search_courses(request, keyword):
     context = {'courses': courses}
 
     return render(request, 'courses/index.html', context=context)
+
+
+def register_items(request, tracking_code):
+    user_order = get_object_or_404(TemporaryOrder, tracking_code=tracking_code)
+    temp_courses = user_order.courses.all()
+
+    for course in temp_courses:
+        course.users.add(request.user)
+        course.save()
+
+    user_order.delete()
+
+    user_cart = get_object_or_404(UserCart, user=request.user)
+
+    for course in user_cart.courses.all():
+        if course in temp_courses:
+            user_cart.courses.remove(course)
+
+    user_cart.save()
+
+
+def delete_failed_temporary_order(tracking_code):
+    user_order = get_object_or_404(TemporaryOrder, tracking_code=tracking_code)
+    user_order.delete()
